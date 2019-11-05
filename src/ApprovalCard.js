229fbf3acc7f55e7941db3e3443547c22e55fc8c
MoyArt Maximus <moyart@gmail.com>
2019-11-04 22:42:55 -0600

    Adding markup

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const ApprovalCard = () => {
-
+  return (
+    <div className="ui card">
+      <div className="content">Some dummy text</div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <button className="button green">Accept</button>
+          <button className="button red">Decline</button>
+        </div>
+      </div>
+    </div>  
+  );
 }
 
 export default ApprovalCard;
