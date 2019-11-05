5a28b2299be6cf51e13d7d6fefaae8f3bd47cc02
MoyArt Maximus <moyart@gmail.com>
2019-11-04 22:50:14 -0600

    Wrapping comming detail with ApprovalCard

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const App = () => {
     
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail author="Max" commentTime="5:30PM" comment="Aguniguna!!" avatar={faker.image.avatar()}/>
+      <ApprovalCard>
+        <CommentDetail author="Max" commentTime="5:30PM" comment="Aguniguna!!" avatar={faker.image.avatar()}/>
+      </ApprovalCard>
       <CommentDetail author="Clau" commentTime="5:34PM" comment="Muy Interesante!" avatar={faker.image.avatar()}/>
       <CommentDetail author="Duck" commentTime="5:55PM" comment="Chaparrooooooo!" avatar={faker.image.avatar()}/>
       <CommentDetail author="MoyArt" commentTime="6:30PM" comment="Bastante Cools!" avatar={faker.image.avatar()}/>
